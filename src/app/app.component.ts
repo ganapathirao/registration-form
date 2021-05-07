e967f12dbad1985df1b89be0a31083c937fb6607
ganapathi <ganeshthota418@gmail.com>
2021-05-07 10:19:48 +0530

    created registration form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validators, NgForm } from '@angular/forms';
+import { MustMatch } from './helpers/must-match.validator';
+import { MatSnackBar } from '@angular/material/snack-bar'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'registration-form';
+  constructor(
+    private fb: FormBuilder,
+    private _snackBar: MatSnackBar
+  ) { }
+
+  // Creating form controls for registration form
+  registerForm = this.fb.group({
+    firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
+    lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
+    mobileNumber: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+    email: ['', [Validators.required, Validators.email]],
+    dob: ['', Validators.required],
+    address: [''],
+    city: [''],
+    state: [''],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    confirmPassword: ['', Validators.required],
+  }, {
+    validator: MustMatch('password', 'confirmPassword')
+  });
+
+  // Getter function to get the form controls
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+
+  // Function to execute on form submit
+  onSubmit(form:any) {
+    if(this.registerForm.invalid)
+      return;
+    
+    this._snackBar.open('Registered Successfully..', '', {
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      duration: 3000,
+      panelClass:'bg-success'
+    });
+    form.resetForm();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from  '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// angular material dependency modules
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
